ac1163a57c3480148e927e00228c17b2234758d2
Jonas Eana <jonas@eana.ro>
2021-10-24 22:08:18 +0200

    Do not allow to delete from the backup bucket

--- iam.tf
+++ iam.tf
@@ -146,7 +146,6 @@ data "aws_iam_policy_document" "s3policy" {
     actions = [
       "s3:PutObject",
       "s3:GetObject",
-      "s3:DeleteObject",
       "s3:PutObjectAcl"
     ]
     principals {
