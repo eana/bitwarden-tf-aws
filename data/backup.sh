c3a2825e8b0ff1438605e29310b0b38cbaac3750
Jonas Eana <jonas@eana.ro>
2021-10-24 21:35:10 +0200

    Fix path

--- data/backup.sh
+++ data/backup.sh
@@ -9,7 +9,7 @@ docker-compose -f /home/ec2-user/bitwarden/compose/docker-compose.yml down
 
 # Back up and upload to S3
 sudo tar cfz "$TODAY"_bitwarden-backup.tar.gz /home/ec2-user/bitwarden/{bitwarden-data,letsencrypt,mysql}
-/usr/local/bin/aws s3 cp "$TODAY"_bitwarden-backup.tar.gz "s3://${bucket}/bitwarden-backup.tar.gz" --sse
+/usr/bin/aws s3 cp "$TODAY"_bitwarden-backup.tar.gz "s3://${bucket}/bitwarden-backup.tar.gz" --sse
 sudo rm "$TODAY"_bitwarden-backup.tar.gz
 
 # Start the app
