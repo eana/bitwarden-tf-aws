39231310f3cb8a945bafb41e71a7d99c6eb0edbe
Jonas Eana <jonas@eana.ro>
2023-06-26 22:03:28 +0200

    Fix legacy version constraint error

--- example/main.tf
+++ example/main.tf
@@ -6,8 +6,14 @@ terraform {
   required_version = ">= 0.13.1"
 
   required_providers {
-    aws   = ">= 3.56.0"
-    local = ">= 1.4"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.56.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 1.4"
+    }
   }
 }
 
--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,13 @@ terraform {
   required_version = ">= 0.13.1"
 
   required_providers {
-    aws   = ">= 3.56.0"
-    local = ">= 1.4"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.56.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 1.4"
+    }
   }
 }
