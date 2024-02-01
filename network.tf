966764636672470aae45134cff2c61d8ab5e033b
Jonas Eana <jonas@eana.ro>
2024-02-01 14:01:45 +0100

    Address checkov's findings

--- network.tf
+++ network.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "this" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS009
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -18,17 +18,10 @@ resource "aws_security_group" "this" {
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS008
-  }
-
-  ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS008
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = local.default_tags
